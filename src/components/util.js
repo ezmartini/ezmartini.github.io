6d3dc67b9dd69c1f90cb2f8317e61134730dd389
ezmartini <em4047@nyu.edu>
2023-01-20 16:34:53 -0500

    Render

--- src/components/util.js
+++ src/components/util.js
@@ -1,8 +1,8 @@
 export const cardClasses = {
   small:
-    "col-lg-3 col-md-5 col-sm-7 p-2  custom-card mb-md-3 mb-sm-3 mb-3 col-7 mx-auto",
+    "col-lg-3 col-md-5 col-sm-7 p-2  custom-card mb-md-3 mb-sm-3 mb-3 col-10 mx-auto",
   large:
-    "col-lg-8 col-md-5 col-sm-7 p-2  custom-card mb-md-3 mb-sm-3 mb-3 col-7 mx-auto",
+    "col-lg-8 col-md-5 col-sm-7 p-2  custom-card mb-md-3 mb-sm-3 mb-3 col-10 mx-auto",
 
   right: " ms-lg-2 ms-md-2",
 
