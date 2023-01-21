0c611975d5a7062a3639207e1706e4c22fd711d7
ezmartini <em4047@nyu.edu>
2023-01-21 17:23:14 -0500

    added links to icons

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row, Container } from "react-bootstrap";
 import { cardClasses } from "./util";
 import { BsBroadcast } from "react-icons/bs";
+import Icon from "../icons/Icon";
 
 // change this name
 export default function Connect() {
   return (
     <>
       <Card className={cardClasses.small + " px-0 + py-0" + " connectCard"}>
-        <Card.Body></Card.Body>
+        <Card.Body>
+          <Container className="justify-content-center">
+            <Row>
+              <Icon name="github" />
+              <Icon name="linkedin" />
+              <Icon name="email" />
+              <Icon name="spotify" />
+            </Row>
+          </Container>
+        </Card.Body>
       </Card>
     </>
   );
--- src/icons/icon.js
+++ src/icons/icon.js
@@ -1,5 +1,57 @@
 import React from "react";
+import { BsGithub, BsEnvelopeFill, BsSpotify } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
 
-export default function icon() {
-  return <div>icon</div>;
+export default function Icon(props) {
+  const icons = {
+    github: {
+      bsIcon: <BsGithub />,
+      color: "#6e5494",
+      name: "GitHub",
+      site: "https://github.com/ezmartini",
+    },
+    linkedin: {
+      bsIcon: <FaLinkedinIn />,
+      color: "#0072b1",
+      name: "LinkedIn",
+      site: "https://www.linkedin.com/in/eduardazm/",
+    },
+
+    email: {
+      bsIcon: <BsEnvelopeFill />,
+      color: "#67B7D1",
+      name: "Email",
+      site: "mailto: eduardazmartini@gmail.com",
+    },
+
+    spotify: {
+      bsIcon: <BsSpotify />,
+      color: "#1DB954",
+      name: "Spotify",
+      site: "https://open.spotify.com/playlist/2Pcv5qlPqXt55DcPC4g3nU",
+    },
+  };
+  return (
+    <>
+      <div className="text-center text-white col-lg-3 p-1 col-md-4 col-sm-3 col-3">
+        <div className="connectDiv">
+          <a class="iconLink" href={icons[props.name].site}>
+            <div
+              className="rounded"
+              style={{
+                backgroundColor: icons[props.name].color,
+                fontSize: "34px",
+              }}
+            >
+              {icons[props.name].bsIcon}
+            </div>
+            <p className="small text-black mt-1" style={{ fontSize: "10px" }}>
+              {" "}
+              {icons[props.name].name}{" "}
+            </p>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
